7a415c1f6e491d1a10ac878948fa6236b048a846
Sakkachin Wongcharoen <rhythmsosad@gmail.com>
2015-07-27 13:43:57 +0700

    RESTful

---
+++ SimpleEvaluation/api/index.php
@@ -0,0 +1,27 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require '/vendor/autoload.php';
+require 'db.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/users', function () {
+    $sql_query = "select `id`,`name`,`surname` from employee";
+    try {
+        $dbCon = getConnection();
+        $stmt   = $dbCon->query($sql_query);
+        $users  = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $dbCon = null;
+        echo '{"users": ' . json_encode($users) . '}';
+    }
+    catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }    
+});
+
+$app->run();
+
--- SimpleEvaluation/index.php
+++ SimpleEvaluation/index.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo phpinfo();
-        ?>
-    </body>
-</html>
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require '/vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+
+$app->run();
